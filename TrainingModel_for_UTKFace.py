cc0c3234b7467deee77e990fee5b044924aca7b7
Aashrit912 <38270269+Aashrit912@users.noreply.github.com>
2023-06-13 13:17:43 -0400

    Add files via upload
    Should be able to traing a neural network model using UTKFace

---
+++ TrainingModel_for_UTKFace.py
@@ -0,0 +1,151 @@
+#importing all packages that may be needed
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+from PIL import Image
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+import numpy as np
+from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import Dataset
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+from sklearn.model_selection import train_test_split
+from matplotlib.legend_handler import HandlerBase
+import matplotlib.patches as mpatches
+import torch.nn.functional as F
+
+
+#Defining a dataset class
+class UTKFaceDataset(Dataset):
+    #Initializes dataset by setting root directory and transformation
+    def __init__(self, root, transform=None):
+        self.root = root
+        self.transform = transform
+        self.images = self.load_images()
+    # reads images files from root directory and reutnrs list of image paths where the file ends with .jpg or .png
+    def load_images(self):
+        image_files = sorted(os.listdir(self.root))
+        images = []
+        for filename in image_files:
+            if filename.endswith('.jpg') or filename.endswith('.png'):
+                image_path = os.path.join(self.root, filename)
+                images.append(image_path)
+        return images
+    def get_class_label(self, filename):
+        # Extract the age, gender, and race from the filename
+        age, gender, race = filename.split('_')[:3]
+        
+        # Assign class label based on gender (0 for male, 1 for female)
+        class_label = 0 if int(gender) == 0 else 1
+        
+        return class_label
+    # Indexes dataset and applies transformation and returns image and tuple
+    def __getitem__(self, index):
+        image_path = self.images[index]
+        image = Image.open(image_path).convert('RGB')
+        
+        if self.transform is not None:
+            image = self.transform(image)
+        
+        class_label = self.get_class_label(os.path.basename(image_path))
+        
+        return image, class_label
+    # returns total number of images
+    def __len__(self):
+        return len(self.images)
+
+
+
+# Set the root directory of the UTKFace dataset
+root_dir = 'C:/Users/aashr/OneDrive/Documents/Research Projects/EmoryREU/UTKFace.tar/UTKFace/UTKFace'
+
+#Defines transformation pipeline by resizing, converting to tensors, and normalizing
+transform = transforms.Compose([
+    transforms.Resize((32, 32)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+# Create an instance of the UTKFaceDataset
+utkface_dataset = UTKFaceDataset(root_dir, transform=transform)
+
+# Create a DataLoader for the UTKFace dataset
+utkface_loader = torch.utils.data.DataLoader(utkface_dataset, batch_size=4, shuffle=True, num_workers=2)
+
+
+class Net(nn.Module):
+    #Initializes layers of network by defining convolutional layers, max-pooling layers, and fully connected layers with appropriate and output sizes
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 2)  # UTKFace dataset has 2 classes: male and female
+    # defines forward pass and returns an output
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+
+#trainset = ImageFolder(root='C:/Users/aashr/OneDrive/Documents/Research Projects/EmoryREU/UTKFace.tar/UTKFace/UTKFace', transform=transform)
+#trainloader = DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+
+#testset = ImageFolder(root='C:/Users/aashr/OneDrive/Documents/Research Projects/EmoryREU/UTKFace.tar/UTKFace/UTKFace', transform=transform)
+#testloader = DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
+
+classes = ('male', 'female')
+
+net = Net()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+# Train
+if __name__ == '__main__':
+    for epoch in range(2):
+        running_loss = 0.0
+        for i, data in enumerate(utkface_loader, 0):
+            inputs, labels = data
+            optimizer.zero_grad()
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            if i % 2000 == 1999:
+                print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+
+    print('Finished Training')
+
+# Test the network on the test data
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in utkface_loader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print('Accuracy of the network on the test images: %.2f %%' % (100 * correct / total))
+
+# Check accuracy for each class
+    class_correct = list(0. for _ in range(len(classes)))
+    class_total = list(0. for _ in range(len(classes)))
