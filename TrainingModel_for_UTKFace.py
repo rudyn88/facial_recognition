bfe885e6799540e63b42b80007489634ec858980
LucaB725 <126108087+LucaB725@users.noreply.github.com>
2023-06-14 11:02:37 -0400

    Update for Easier Class Switching

--- TrainingModel_for_UTKFace.py
+++ TrainingModel_for_UTKFace.py
@@ -1,8 +1,9 @@
-#importing all packages that may be needed
+# importing all packages that may be needed
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
+import tensorflow as tf
 from PIL import Image
 import torch
 import torch.nn as nn
@@ -25,13 +26,14 @@ import matplotlib.patches as mpatches
 import matplotlib.lines as mlines
 
 
-#Defining a dataset class
+# Defining a dataset class
 class UTKFaceDataset(Dataset):
-    #Initializes dataset by setting root directory and transformation
+    # Initializes dataset by setting root directory and transformation
     def __init__(self, root, transform=None):
         self.root = root
         self.transform = transform
         self.images = self.load_images()
+
     # reads images files from root directory and reutnrs list of image paths where the file ends with .jpg or .png
     def load_images(self):
         image_files = sorted(os.listdir(self.root))
@@ -41,35 +43,37 @@ class UTKFaceDataset(Dataset):
                 image_path = os.path.join(self.root, filename)
                 images.append(image_path)
         return images
+
     def get_class_label(self, filename):
         # Extract the age, gender, and race from the filename
         age, gender, race = filename.split('_')[:3]
-        
         # Assign class label based on gender (0 for male, 1 for female)
-        class_label = 0 if int(gender) == 0 else 1
-        
+        #class_label = 0 if int(gender) == 0 else 1
+        class_label = int(race) #just change this to age, gender, or race
+
         return class_label
+
     # Indexes dataset and applies transformation and returns image and tuple
     def __getitem__(self, index):
         image_path = self.images[index]
         image = Image.open(image_path).convert('RGB')
-        
+
         if self.transform is not None:
             image = self.transform(image)
-        
+
         class_label = self.get_class_label(os.path.basename(image_path))
-        
+
         return image, class_label
+
     # returns total number of images
     def __len__(self):
         return len(self.images)
 
 
-
 # Set the root directory of the UTKFace dataset
-root_dir = 'C:/Users/aashr/OneDrive/Documents/Research Projects/EmoryREU/UTKFace.tar/UTKFace/UTKFace'
+root_dir = 'C:/Users/lucab/Downloads/UTKFace'
 
-#Defines transformation pipeline by resizing, converting to tensors, and normalizing
+# Defines transformation pipeline by resizing, converting to tensors, and normalizing
 transform = transforms.Compose([
     transforms.Resize((32, 32)),
     transforms.ToTensor(),
@@ -83,8 +87,10 @@ utkface_dataset = UTKFaceDataset(root_dir, transform=transform)
 utkface_loader = torch.utils.data.DataLoader(utkface_dataset, batch_size=4, shuffle=True, num_workers=2)
 
 
+
+
 class Net(nn.Module):
-    #Initializes layers of network by defining convolutional layers, max-pooling layers, and fully connected layers with appropriate and output sizes
+    # Initializes layers of network by defining convolutional layers, max-pooling layers, and fully connected layers with appropriate and output sizes
     def __init__(self):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
@@ -92,7 +98,8 @@ class Net(nn.Module):
         self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 2)  # UTKFace dataset has 2 classes: male and female
+        self.fc3 = nn.Linear(84, 5)  # UTKFace dataset has 2 classes: male and female, 5 race classes, a lot of age classes
+
     # defines forward pass and returns an output
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
@@ -104,14 +111,14 @@ class Net(nn.Module):
         return x
 
 
+# trainset = ImageFolder(root='C:/Users/aashr/OneDrive/Documents/Research Projects/EmoryREU/UTKFace.tar/UTKFace/UTKFace', transform=transform)
+# trainloader = DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
 
-#trainset = ImageFolder(root='C:/Users/aashr/OneDrive/Documents/Research Projects/EmoryREU/UTKFace.tar/UTKFace/UTKFace', transform=transform)
-#trainloader = DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
-
-#testset = ImageFolder(root='C:/Users/aashr/OneDrive/Documents/Research Projects/EmoryREU/UTKFace.tar/UTKFace/UTKFace', transform=transform)
-#testloader = DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
+# testset = ImageFolder(root='C:/Users/aashr/OneDrive/Documents/Research Projects/EmoryREU/UTKFace.tar/UTKFace/UTKFace', transform=transform)
+# testloader = DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
 
 classes = ('male', 'female')
+
 
 net = Net()
 
@@ -138,8 +145,7 @@ if __name__ == '__main__':
     print('Finished Training')
     truth = torch.tensor([])
     pred = torch.tensor([])
-    
-# Test the network on the test data
+    # Test the network on the test data
     correct = 0
     total = 0
     with torch.no_grad():
@@ -152,17 +158,23 @@ if __name__ == '__main__':
             truth = torch.cat((truth, labels), 0)
             pred = torch.cat((pred, predicted), 0)
 
+
     print('Accuracy of the network on the test images: %.2f %%' % (100 * correct / total))
 
-# Check accuracy for each class
+    # Check accuracy for each class
     class_correct = list(0. for _ in range(len(classes)))
     class_total = list(0. for _ in range(len(classes)))
-    
-    RocCurveDisplay.from_predictions(truth, pred, color="darkorange")
-    plt.plot([0, 1], [0, 1], "k--", label="chance level (AUC = 0.5)")
-    plt.axis("square")
-    plt.xlabel("False Positive Rate")
-    plt.ylabel("True Positive Rate")
-    plt.title("ROC curve")
-    plt.legend()
+
+
+#    RocCurveDisplay.from_predictions(truth, pred, color="darkorange")
+#    plt.plot([0, 1], [0, 1], "k--", label="chance level (AUC = 0.5)")
+#    plt.axis("square")
+#    plt.xlabel("False Positive Rate")
+#    plt.ylabel("True Positive Rate")
+#    plt.title("ROC curve")
+#    plt.legend()
+#    plt.show()
+
+    plt.figure()
+    sns.barplot()
     plt.show()
